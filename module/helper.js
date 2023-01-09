9dec95392340c9d335cbaa768d91bb92d4ac2697
niklasfranz <nf.app@icloud.com>
2023-01-09 04:33:02 +0100

    added item roll icon and functionality

--- module/helper.js
+++ module/helper.js
@@ -377,26 +377,139 @@ export class EntitySheetHelper {
           let results_html = '';
 
           let rollMessage = `${characterName} würfelt auf <b>${skillLabel}</b>, sein ${skillLabel}-Wert beträgt <a class="inline-roll inline-result" data-tooltip="${skillLabel}">${skillValue}${modifierString}</a>. Er würfelt <a class="inline-roll inline-result" data-tooltip="1d100"><i class="fas fa-dice-d20"></i>${rollHit.result}</a> und `;
-          let successState = "";
+          let successStateLabel = "";
           if(rollHit.total <= skillValue + modifier){
               console.log("Success");
               
               if(rollHit.total <= ((skillValue + modifier) / 10)){
                   // results_html = `${characterName} has a critical success with ${rollHit.result} ` + "<" + ` ${actor.system.attributes[category][skill].value} ${actor.system.attributes[category][skill].label}`
                   results_html = rollMessage + ` hat damit einen <b>kritischen Erfolg</b>!`;
-                  successState = "Kritischer Erfolg!"
+                  successStateLabel = "Kritischer Erfolg!"
               }else{
                   // results_html = `${characterName} is successful with ${rollHit.result}  + "<" + ${actor.system.attributes[category][skill].value} ${actor.system.attributes[category][skill].label}`
                   results_html = rollMessage + ` ist damit <b>erfolgreich</b>! `;
-                  successState = "Erfolg!"
+                  successStateLabel = "Erfolg!"
               }
           }else{
               console.log("Fail");
               // results_html = `${characterName} failed with ${rollHit.result} ` + ">" + ` ${actor.system.attributes[category][skill].value}`;
               results_html = rollMessage + ` ist damit <b>nicht erfolgreich</b>!`;
-              successState = "Fehlschlag!"
+              successStateLabel = "Fehlschlag!"
           }
-          let rollDisplay = `</br></br><div class="dice-roll"><div class="dice-result"><div class="dice-formula">1d100: <i class="fas fa-dice-d20"></i>${rollHit.result}</div><div class="dice-tooltip"><section class="tooltip-part"><div class="dice"><header class="part-header flexrow"><span class="part-formula">1d100</span><span class="part-total">${rollHit.result}</span></header><ol class="dice-rolls"><li class="roll die d100">${rollHit.result}</li></ol></div></section></div><h4 class="dice-total">${successState}</h4></div></div></div>`
+          let rollDisplay = `</br></br><div class="dice-roll"><div class="dice-result"><div class="dice-formula">1d100: <i class="fas fa-dice-d20"></i>${rollHit.result}</div><div class="dice-tooltip"><section class="tooltip-part"><div class="dice"><header class="part-header flexrow"><span class="part-formula">1d100</span><span class="part-total">${rollHit.result}</span></header><ol class="dice-rolls"><li class="roll die d100">${rollHit.result}</li></ol></div></section></div><h4 class="dice-total">${successStateLabel}</h4></div></div></div>`
+          results_html = results_html + rollDisplay
+
+          ChatMessage.create({
+              user: game.user._id,
+              sound: CONFIG.sounds.dice,
+              //speaker: ChatMessage.getSpeaker({token: actor}),
+              content: results_html
+          });
+      }
+
+      const form = '<form><label>Modifier: <input name="modifier" type="string" value="0"/></label>' + '</br></br>Ein positiver Modifier erleichtert den Wurf, ein negativer Modifier erschwert ihn.</br></br>' + 
+      '</form>';
+
+      new Dialog({
+          title: "Skill Check: Gib hier den Roll-Modifier vom GM ein",
+          content: form,
+          buttons: {
+              submit: {label: "Submit", callback: handleSubmit},
+              cancel: {label: "Cancel"},
+          },
+      }).render(true);
+      return null;
+    }
+
+    static onAranthozItemRoll(event) {
+      event.preventDefault();
+      console.log(event)
+      const button = event.currentTarget;
+      const itemID = button.getAttribute("data-id")
+      const itemOwnerID = button.getAttribute("data-actor-id")
+
+      const itemOwner = Actor.get(itemOwnerID)
+      const item = itemOwner.items.get(itemID)
+      const skillKey = item.system.skill
+
+      const skill = itemOwner.system.attributes['handeln'][skillKey]
+      if (!skill) {
+        ui.notifications.error('Linked skill "' + skillKey + '" not found for actor ' + itemOwner.name + '!');
+        return
+      }
+
+      const skillValue = skill.value
+      const skillLabel = skill.label
+      
+      const characterName = itemOwner.name
+
+      if (!skillValue) {
+        ui.notifications.error("This attribute has no attribute value!");
+        return
+      }
+      if (!skillLabel) {
+        ui.notifications.warn("This attribute has no attribute label!")
+        skillLabel = "Attribut"
+      }
+
+      console.log(skillValue)
+
+      async function handleSubmit(html) {
+          const formElement = html[0].querySelector('form');
+          const formData = new FormDataExtended(formElement);
+          const formDataObject = formData.toObject();
+
+          const modifier = parseInt(formDataObject.modifier);
+          const modifiedValue = skillValue + modifier
+
+          // depending on if there was a value != 0 the modifier will be shown in the chat message by appending the variable modifierString to it
+
+          let modifierString = '';
+          if (modifier > 0) {modifierString = " + " + modifier + " = " + modifiedValue}; 
+          if (modifier < 0) {modifierString = modifier + " = " + modifiedValue};
+
+          let rollHit = await new Roll("1d100").evaluate();
+          let results_html = '';
+
+          let rollMessage = `${characterName} würfelt auf <b>${skillLabel}</b>, sein ${skillLabel}-Wert beträgt <a class="inline-roll inline-result" data-tooltip="${skillLabel}">${skillValue}${modifierString}</a>. Er würfelt <a class="inline-roll inline-result" data-tooltip="1d100"><i class="fas fa-dice-d20"></i>${rollHit.result}</a> und `;
+          let successStateLabel = "";
+          let successState = 0;
+          if(rollHit.total <= skillValue + modifier){
+              console.log("Success");
+              console.log(item)
+
+              var itemBaseDamageFormula = item.system.attributes['damage']['base'].value
+              console.log(itemBaseDamageFormula)
+              if (itemBaseDamageFormula) {
+                let rollDamage = await new Roll(itemBaseDamageFormula).evaluate();
+                console.log(rollDamage.total)
+              }              
+              
+              if(rollHit.total <= ((skillValue + modifier) / 10)){
+                  // results_html = `${characterName} has a critical success with ${rollHit.result} ` + "<" + ` ${actor.system.attributes[category][skill].value} ${actor.system.attributes[category][skill].label}`
+                  results_html = rollMessage + ` hat damit einen <b>kritischen Erfolg</b>!`;
+                  successStateLabel = "Kritischer Erfolg!"
+                  successState = 2;
+              }else{
+                  // results_html = `${characterName} is successful with ${rollHit.result}  + "<" + ${actor.system.attributes[category][skill].value} ${actor.system.attributes[category][skill].label}`
+                  results_html = rollMessage + ` ist damit <b>erfolgreich</b>! `;
+                  successStateLabel = "Erfolg!"
+                  successState = 1;
+              }
+
+
+
+
+
+
+          }else{
+              console.log("Fail");
+              // results_html = `${characterName} failed with ${rollHit.result} ` + ">" + ` ${actor.system.attributes[category][skill].value}`;
+              results_html = rollMessage + ` ist damit <b>nicht erfolgreich</b>!`;
+              successStateLabel = "Fehlschlag!"
+              successState = 0;
+          }
+          let rollDisplay = `</br></br><div class="dice-roll"><div class="dice-result"><div class="dice-formula">1d100: <i class="fas fa-dice-d20"></i>${rollHit.result}</div><div class="dice-tooltip"><section class="tooltip-part"><div class="dice"><header class="part-header flexrow"><span class="part-formula">1d100</span><span class="part-total">${rollHit.result}</span></header><ol class="dice-rolls"><li class="roll die d100">${rollHit.result}</li></ol></div></section></div><h4 class="dice-total">${successStateLabel}</h4></div></div></div>`
           results_html = results_html + rollDisplay
 
           ChatMessage.create({
@@ -419,27 +532,7 @@ export class EntitySheetHelper {
           },
       }).render(true);
 
-      // Use the actor for rollData so that formulas are always in reference to the parent actor.
-      // const rollData = this.actor.getRollData();
-      // let formula = button.closest(".attribute").querySelector(".attribute-value")?.value;
-  
-      // // If there's a formula, attempt to roll it.
-      // if ( formula ) {
-      //   let replacement = null;
-      //   if ( formula.includes('@item.') && this.item ) {
-      //     let itemName = this.item.name.slugify({strict: true}); // Get the machine safe version of the item name.
-      //     replacement = !!shorthand ? `@items.${itemName}.` : `@items.${itemName}.attributes.`;
-      //     formula = formula.replace('@item.', replacement);
-      //   }
-  
-      //   // Create the roll and the corresponding message
-      //   let r = new Roll(formula, rollData);
-      //   return r.toMessage({
-      //     user: game.user.id,
-      //     speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      //     flavor: `${chatLabel}`
-      //   });
-      // }
+    
 
       return null;
     }
--- module/item-sheet.js
+++ module/item-sheet.js
@@ -5,6 +5,104 @@ import {ATTRIBUTE_TYPES} from "./constants.js";
  * Extend the basic ItemSheet with some very simple modifications
  * @extends {ItemSheet}
  */
+export class AranthozItemSheet extends ItemSheet {
+
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["worldbuilding", "sheet", "item"],
+      template: "systems/aranthoz/templates/aranthoz/item-sheet.html",
+      width: 520,
+      height: 480,
+      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
+      scrollY: [".attributes"],
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async getData(options) {
+    const context = await super.getData(options);
+    EntitySheetHelper.getAttributeData(context.data);
+    context.systemData = context.data.system;
+    console.log(context)
+
+    var itemOwnerId = EntitySheetHelper.getItemOwnerId(context.data._id);
+    console.log("itemOwnerId: " + itemOwnerId)
+    context.ownerId = itemOwnerId
+
+    context.isOfTypeItem = context.data.type === "item";
+    console.log(context.isOfTypeItem)
+
+    if (itemOwnerId) {
+      // add an ownerAttributes property to the context object that holds an object {"value": skill, "selected": selected} for all available attributes of the owner
+      var ownerAttributes = []
+      for (var a of EntitySheetHelper.getActorAttributes(itemOwnerId)) {
+        var selected = ""
+        if (a == context.systemData.skill) {
+          selected = " selected"
+        }
+        ownerAttributes.push({"value": a, "selected": selected})
+        console.log(ownerAttributes)
+      }
+      context.ownerAttributes = ownerAttributes
+    }
+    
+    console.log(context)
+    
+    context.dtypes = ATTRIBUTE_TYPES;
+    context.descriptionHTML = await TextEditor.enrichHTML(context.systemData.description, {
+      secrets: this.document.isOwner,
+      async: true
+    });
+
+    return context;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if ( !this.isEditable ) return;
+
+    // Attribute Management
+    html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
+    html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
+    html.find(".attributes").on("click", "a.attribute-roll", EntitySheetHelper.onAttributeRoll.bind(this));
+    html.find(".skill").on("change", ".skill-select", EntitySheetHelper.onSkillChange.bind(this))
+
+    // Add draggable for Macro creation
+    html.find(".attributes a.attribute-roll").each((i, a) => {
+      a.setAttribute("draggable", true);
+      a.addEventListener("dragstart", ev => {
+        let dragData = ev.currentTarget.dataset;
+        ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
+      }, false);
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  _getSubmitData(updateData) {
+    let formData = super._getSubmitData(updateData);
+    formData = EntitySheetHelper.updateAttributes(formData, this.object);
+    formData = EntitySheetHelper.updateGroups(formData, this.object);
+    return formData;
+  }
+}
+
+
+
+
+/**
+ * Extend the basic ItemSheet with some very simple modifications
+ * @extends {ItemSheet}
+ */
 export class SimpleItemSheet extends ItemSheet {
 
   /** @inheritdoc */
@@ -72,97 +170,5 @@ export class SimpleItemSheet extends ItemSheet {
 /* -------------------------------------------- */
 /* -------------------------------------------- */
 /* -------------------------------------------- */
-
-/**
- * Extend the basic ItemSheet with some very simple modifications
- * @extends {ItemSheet}
- */
-export class AranthozItemSheet extends ItemSheet {
-
-  /** @inheritdoc */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["worldbuilding", "sheet", "item"],
-      template: "systems/aranthoz/templates/aranthoz/item-sheet.html",
-      width: 520,
-      height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
-      scrollY: [".attributes"],
-    });
-  }
-
-  /* -------------------------------------------- */
-
-  /** @inheritdoc */
-  async getData(options) {
-    const context = await super.getData(options);
-    EntitySheetHelper.getAttributeData(context.data);
-    context.systemData = context.data.system;
-    console.log(context)
-
-    var itemOwnerId = EntitySheetHelper.getItemOwnerId(context.data._id);
-    console.log("itemOwnerId: " + itemOwnerId)
-    context.ownerId = itemOwnerId
-
-    if (itemOwnerId) {
-      // add an ownerAttributes property to the context object that holds an object {"value": skill, "selected": selected} for all available attributes of the owner
-      var ownerAttributes = []
-      for (var a of EntitySheetHelper.getActorAttributes(itemOwnerId)) {
-        var selected = ""
-        if (a == context.systemData.skill) {
-          selected = " selected"
-        }
-        ownerAttributes.push({"value": a, "selected": selected})
-        console.log(ownerAttributes)
-      }
-      context.ownerAttributes = ownerAttributes
-    }
-    
-    console.log(context)
-    
-    context.dtypes = ATTRIBUTE_TYPES;
-    context.descriptionHTML = await TextEditor.enrichHTML(context.systemData.description, {
-      secrets: this.document.isOwner,
-      async: true
-    });
-
-    return context;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @inheritdoc */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    // Everything below here is only needed if the sheet is editable
-    if ( !this.isEditable ) return;
-
-    // Attribute Management
-    html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
-    html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
-    html.find(".attributes").on("click", "a.attribute-roll", EntitySheetHelper.onAttributeRoll.bind(this));
-    html.find(".skill").on("change", ".skill-select", EntitySheetHelper.onSkillChange.bind(this))
-
-    // Add draggable for Macro creation
-    html.find(".attributes a.attribute-roll").each((i, a) => {
-      a.setAttribute("draggable", true);
-      a.addEventListener("dragstart", ev => {
-        let dragData = ev.currentTarget.dataset;
-        ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
-      }, false);
-    });
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  _getSubmitData(updateData) {
-    let formData = super._getSubmitData(updateData);
-    formData = EntitySheetHelper.updateAttributes(formData, this.object);
-    formData = EntitySheetHelper.updateGroups(formData, this.object);
-    return formData;
-  }
-}
 
 
